507bac88e64677f3065a018db1bf8370be630258
LeoGrochoski <leogrochoski10@gmail.com>
2024-10-24 20:22:37 -0300

    feat: refatorando desafio criando funcao para carregar e chamar os dados

--- numpy_class_1_challenge.py
+++ numpy_class_1_challenge.py
@@ -1,9 +1,17 @@
 import numpy as np
+import csv
 
-data = './numpy-dados/citrus.csv'
+path = './numpy-dados/citrus.csv'
 
 # Dont forget to understand the range of the array (columns and rows).
 # The first columns is an string columns, so is necessary to skip that.
 
-data = np.loadtxt(data, delimiter=',', usecols=np.arange(1,6,1), skiprows=1)
-print(data)
+def load_data(file: csv):
+    data = np.loadtxt(file, delimiter=',', usecols=np.arange(1,6,1), skiprows=1)
+    print(data)
+
+def main():
+    load_data(path)
+
+if __name__ == "__main__":
+    main()
